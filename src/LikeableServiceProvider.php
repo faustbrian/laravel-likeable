ecce1cd33163289095c7b476e2502bf16afcd4ae
Brian Faust <hello@brianfaust.de>
2016-12-03 19:32:11 +0200

    :hammer: Start making use of some PHP 7.1 goodies

--- src/LikeableServiceProvider.php
+++ src/LikeableServiceProvider.php
@@ -9,6 +9,8 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace BrianFaust\Likeable;
 
 use BrianFaust\ServiceProvider\ServiceProvider;
@@ -18,7 +20,7 @@ class LikeableServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(): void
     {
         $this->publishMigrations();
     }
@@ -28,7 +30,7 @@ class LikeableServiceProvider extends ServiceProvider
      *
      * @return string
      */
-    public function getPackageName()
+    public function getPackageName(): string
     {
         return 'likeable';
     }
--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -9,6 +9,8 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace BrianFaust\Tests\Likeable;
 
 use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
